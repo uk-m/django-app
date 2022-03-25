242f07df2cc012e434fe385b45fc361398dd81a2
uk-m <mymk426@kyf.biglobe.ne.jp>
2022-03-25 22:32:52 +0900

    first commit

---
+++ myapp/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Category(models.Model):
+  name = models.CharField('カテゴリ名', max_length=20)
+  name_en = models.CharField('カテゴリ名英語', max_length=10)
+  created_at = models.DateTimeField(auto_now_add=True)
+  update_at = models.DateTimeField(auto_now=True)
+
+  def post_count(self):
+    n = Post.objects.filter(category = self).count()
+    return n
+
+  def __str__(self):
+    return self.name
+
+class Post(models.Model):
+  author = models.ForeignKey(User, on_delete=models.PROTECT, blank=False)
+  title = models.CharField('タイトル', max_length=50)
+  content = models.TextField('内容', max_length=990)
+  category = models.ForeignKey('Category', on_delete=models.PROTECT)
+  thumbnail = models.ImageField(upload_to='images/', blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  update_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.title
+
+class Like(models.Model):
+  post = models.ForeignKey(Post, verbose_name="投稿", on_delete=models.PROTECT)
+  user = models.ForeignKey(User, verbose_name="likeしたユーザー", on_delete=models.PROTECT)
