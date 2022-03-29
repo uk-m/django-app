3aebc861f82b70fc301c7ffce67e2fd2e6235cd8
uk-m <mymk426@kyf.biglobe.ne.jp>
2022-03-29 18:09:07 +0900

    aws_s3

--- myproject/settings.py
+++ myproject/settings.py
@@ -140,6 +140,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'myapp:login'
 LOGIN_REDIRECT_URL = 'myapp:index'
 
+AWS_STORAGE_BUCKET_NAME = 'django999'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
 AWS_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
